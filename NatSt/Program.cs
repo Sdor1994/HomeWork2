634dc1bab94e96b751e9df926516c99ebc8861fd
semen <semen.dorohov@yandex.ru>
2023-01-27 21:12:34 +0300

    6 commit

---
+++ NatSt/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число");
+int B = int.Parse(Console.ReadLine());
+int i = 1;
+int Q = 1;
+while (i <= B)
+{
+    Q = Q * A;
+    i++;
+}
+Console.WriteLine(A + " В степени "+ B +" = "+Q );
