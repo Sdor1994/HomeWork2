f485546cf98b1c38484bbd82fd240b2b1160a2de
semen <semen.dorohov@yandex.ru>
2023-01-27 20:56:16 +0300

    first commit

---
+++ ЧетныеКубы/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine());
+int i = 1;
+int Q = 0;
+while (i <= A)
+{
+    Q = i*i*i;
+    if (Q%2 ==0 ) Console.WriteLine("Куб от четный "+i+" = "+Q );
+    i++;
+}
