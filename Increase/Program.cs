c7ab496036a24758fea8862faba6150dff20a39b
semen <semen.dorohov@yandex.ru>
2023-01-27 21:12:46 +0300

    7 commit

---
+++ Increase/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine());
+int i = 1;
+int Q = 1;
+while (i <= A)
+{
+    Q = Q * i ;
+    i++ ;
+}
+Console.WriteLine("Произведение чисел от 1 до "+ A +" = "+Q );
