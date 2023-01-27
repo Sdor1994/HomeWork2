431d2ad660c174da65b269a68258cdede01478c7
semen <semen.dorohov@yandex.ru>
2023-01-27 21:12:23 +0300

    5 commit

---
+++ SumDigitDoA/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine());
+int i = 1;
+int Q = 0;
+while (i <= A)
+{
+    Q = Q + i;
+    i++;
+}
+Console.WriteLine("Сумма чисел от 1 до "+ A +" = "+Q );
