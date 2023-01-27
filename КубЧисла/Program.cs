2b1a72328ee70188747de48315a00f7ec2ee0a1b
semen <semen.dorohov@yandex.ru>
2023-01-27 21:11:38 +0300

    Free commit

---
+++ КубЧисла/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine());
+int i = 1;
+int Q = 0;
+while (i <= A)
+{
+    Q = i*i*i;
+    Console.WriteLine("Куб от "+i+" = "+Q );
+    i++;
+}
