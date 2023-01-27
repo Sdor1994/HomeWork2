c9c072549af0cf351608cc16befbb1e8ae69cc28
semen <semen.dorohov@yandex.ru>
2023-01-27 21:13:33 +0300

    10 commit

---
+++ 0-1Array/Program.cs
@@ -0,0 +1,23 @@
+﻿void FillMas(int[] arr){
+    for (int i = 0; i < arr.Length; i++)
+    {
+        int num = new Random().Next(2);
+        arr[i] = num;
+    }
+}
+
+void PrintMas(int[] arr){
+    foreach (int item in arr)
+    {
+        System.Console.Write(item + " ");
+    }
+    System.Console.WriteLine();
+}
+
+
+Console.Write("Введите размерность массива: ");
+int k = int.Parse(Console.ReadLine()?? "");
+int[] arr = new int[k];
+FillMas(arr);
+PrintMas(arr);
+System.Console.WriteLine(arr);
