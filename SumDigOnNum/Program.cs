5c9a146bce39d53e971ae6a8fa156637bb509369
semen <semen.dorohov@yandex.ru>
2023-01-27 21:12:08 +0300

    4 commit

---
+++ SumDigOnNum/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число");
+int a = int.Parse(Console.ReadLine());
+int b = a;
+int s = 0;
+ while (a > 0)
+ {
+ 
+     s = s + a % 10;
+     a = a /10 ;
+ 
+ }
+ Console.WriteLine("Сумма чифр в числе "+b+" = "+s);
